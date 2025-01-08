049bb3320d045340f641a3a431f493ddc37cd70d
james5635 <brorojame2029@gmail.com>
2025-01-08 13:00:38 +0700

    add license

--- x.sh
+++ x.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+
+# Copyright (C) 2025 james5635
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 set -e
 build() {
-  mkdir -p build
-  cd build/
-  qmake6 ../calculator.pro
-  # qmake ../calculator.pro
-  make
+    mkdir -p build
+    cd build/
+    qmake6 ../calculator.pro
+    # qmake ../calculator.pro
+    make
 }
 clean() {
-  rm -rf ./build
+    rm -rf ./build
 }
 run() {
-  build
-  ./calculator
+    build
+    ./calculator
 }
 
 "$@"
